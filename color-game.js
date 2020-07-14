e9a30b7f844060fac374f24ed314b1aa657accad
Matt Lisle <matthew.j.lisle@gmail.com>
2020-07-13 20:34:46 -0700

    refactor: app as singleton

--- color-game.js
+++ color-game.js
@@ -1,121 +1,146 @@
-const nColorsHard = 6
-const nColorsEasy = 3
 
-const bodyColor = '#232323'
-const headerColor = 'steelblue'
+const ColorGame = (function () {
+  'use strict'
 
-const resetGiveUpText = 'New Colors'
-const resetAfterWinText = 'Play Again'
+  const settings = {
+    nColorsHard: 6,
+    nColorsEasy: 3,
+    bodyColor: '#232323',
+    headerColor: 'steelblue',
+    resetGiveUpText: 'New Colors',
+    resetAfterWinText: 'Play Again',
+    squares: document.querySelectorAll('.square'),
+    colorDisplay: document.querySelector('#color-display'),
+    messageDisplay: document.querySelector('#message'),
+    gameHeader: document.querySelector('h1')
+  }
 
-let nColorsActive = nColorsHard
-let colors = generateColors(nColorsActive)
+  Object.freeze(settings)
 
-const squares = document.querySelectorAll('.square')
-const colorDisplay = document.querySelector('#color-display')
-let goalColor = pickColor()
-const messageDisplay = document.querySelector('#message')
-const h1 = document.querySelector('h1')
-const resetButton = document.querySelector('#reset')
+  const state = new GameState()
 
-const buttonNodes = document.querySelectorAll('.difficulty-btn')
-const difficultyButtons = [
-  new DifficultyButton(buttonNodes[0], nColorsEasy),
-  new DifficultyButton(buttonNodes[1], nColorsHard)
-]
+  const resetButton = document.querySelector('#reset')
 
-init()
+  const difficultyButtons = (function () {
+    const buttonNodes = document.querySelectorAll('.difficulty-btn')
+    return [
+      new DifficultyButton(buttonNodes[0], settings.nColorsEasy),
+      new DifficultyButton(buttonNodes[1], settings.nColorsHard)
+    ]
+  })()
 
-function init () {
-  if (colors.length !== nColorsActive) throw new Error('Different number of colors vs. squares')
+  function init () {
+    if (state.colors.length !== settings.nColorsHard) {
+      throw new Error('Different number of colors vs. squares')
+    }
 
-  colorDisplay.textContent = goalColor
+    state.pickColor()
 
-  setSquareColors()
+    settings.colorDisplay.textContent = state.goalColor
 
-  squares.forEach(square => {
-    square.addEventListener(
-      'click',
-      function () {
-        const clickedColor = this.style.backgroundColor
-        if (clickedColor === goalColor) {
-          messageDisplay.textContent = 'Correct!'
-          resetButton.textContent = resetAfterWinText
-          setAllToWinningColor(clickedColor)
-        } else {
-          this.style.backgroundColor = bodyColor
-          messageDisplay.textContent = 'Try again'
+    setSquareColors()
+
+    resetButton.textContent = settings.resetGiveUpText
+
+    bindUiActions()
+  }
+
+  function GameState () {
+    this.nColorsActive = settings.nColorsHard
+    this.colors = generateColors(settings.nColorsHard)
+    this.goalColor = undefined
+  }
+
+  GameState.prototype.pickColor = function () {
+    const index = Math.floor(Math.random() * this.nColorsActive)
+    this.goalColor = this.colors[index]
+  }
+
+  function bindUiActions () {
+    settings.squares.forEach(square => {
+      square.addEventListener(
+        'click',
+        function () {
+          const clickedColor = this.style.backgroundColor
+          if (clickedColor === state.goalColor) {
+            settings.messageDisplay.textContent = 'Correct!'
+            resetButton.textContent = settings.resetAfterWinText
+            setAllToWinningColor(clickedColor)
+          } else {
+            this.style.backgroundColor = settings.bodyColor
+            settings.messageDisplay.textContent = 'Try again'
+          }
         }
+      )
+    })
+
+    resetButton.addEventListener('click', resetGame)
+
+    difficultyButtons.forEach(button => {
+      button.node.addEventListener(
+        'click',
+        function () {
+          button.activate()
+          difficultyButtons.forEach(other => {
+            if (other.nColors !== state.nColorsActive) other.deactivate()
+          })
+        }
+      )
+    })
+  }
+
+  function setAllToWinningColor (color) {
+    settings.squares.forEach(square => { square.style.backgroundColor = color })
+    settings.gameHeader.style.backgroundColor = color
+  }
+
+  function generateColors (nColors) {
+    const arr = Array(nColors).fill()
+    return arr.map(_ => {
+      const rgbArray = Array(3).fill().map(_ => Math.floor(Math.random() * 256))
+      return `rgb(${rgbArray.join(', ')})`
+    })
+  }
+
+  function setSquareColors () {
+    for (let i = 0; i < settings.nColorsHard; i++) {
+      if (i >= state.nColorsActive) {
+        settings.squares[i].style.display = 'none'
+      } else {
+        settings.squares[i].style.backgroundColor = state.colors[i]
+        settings.squares[i].style.display = 'block'
       }
-    )
-  })
-
-  resetButton.textContent = resetGiveUpText
-
-  resetButton.addEventListener('click', resetGame)
-
-  difficultyButtons.forEach(button => {
-    button.node.addEventListener(
-      'click',
-      function () {
-        button.activate()
-        difficultyButtons.forEach(other => {
-          if (other.nColors !== nColorsActive) other.deactivate()
-        })
-      }
-    )
-  })
-}
-
-function setAllToWinningColor (color) {
-  squares.forEach(square => { square.style.backgroundColor = color })
-  h1.style.backgroundColor = color
-}
-
-function pickColor () {
-  const index = Math.floor(Math.random() * nColorsActive)
-  return colors[index]
-}
-
-function generateColors (nColors) {
-  const arr = Array(nColors).fill()
-  return arr.map(_ => {
-    const rgbArray = Array(3).fill().map(_ => Math.floor(Math.random() * 256))
-    return `rgb(${rgbArray.join(', ')})`
-  })
-}
-
-function setSquareColors () {
-  for (let i = 0; i < nColorsHard; i++) {
-    if (i >= nColorsActive) {
-      squares[i].style.display = 'none'
-    } else {
-      squares[i].style.backgroundColor = colors[i]
-      squares[i].style.display = 'block'
     }
   }
-}
 
-function resetGame () {
-  colors = generateColors(nColorsActive)
-  goalColor = pickColor()
-  colorDisplay.textContent = goalColor
-  setSquareColors()
-  h1.style.backgroundColor = headerColor
-  resetButton.textContent = resetGiveUpText
-  messageDisplay.textContent = ''
-}
+  function resetGame () {
+    state.colors = generateColors(state.nColorsActive)
+    state.pickColor()
+    settings.colorDisplay.textContent = state.goalColor
+    setSquareColors()
+    settings.gameHeader.style.backgroundColor = settings.headerColor
+    resetButton.textContent = settings.resetGiveUpText
+    settings.messageDisplay.textContent = ''
+  }
 
-function DifficultyButton (node, difficultyLevel) {
-  this.node = node
-  this.nColors = difficultyLevel
-}
+  function DifficultyButton (node, difficultyLevel) {
+    this.node = node
+    this.nColors = difficultyLevel
+  }
 
-DifficultyButton.prototype.activate = function () {
-  this.node.classList.add('selected')
-  nColorsActive = this.nColors
-  resetGame()
-}
+  DifficultyButton.prototype.activate = function () {
+    this.node.classList.add('selected')
+    state.nColorsActive = this.nColors
+    resetGame()
+  }
 
-DifficultyButton.prototype.deactivate = function () {
-  this.node.classList.remove('selected')
-}
+  DifficultyButton.prototype.deactivate = function () {
+    this.node.classList.remove('selected')
+  }
+
+  return {
+    init: init
+  }
+})()
+
+ColorGame.init()
